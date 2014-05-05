761bb7033799fad1169ccc446b941903dcac2533
gorka ercilla <ercillagorka@gmail.com>
2014-05-05 19:38:14 +0200

    versionCode="38"

--- RssJarioa/src/com/gorka/rssjarioa/AppLehen.java
+++ RssJarioa/src/com/gorka/rssjarioa/AppLehen.java
@@ -4,7 +4,6 @@ package com.gorka.rssjarioa;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseInstallation;
 import com.parse.PushService;
 
 public class AppLehen extends Application {
@@ -15,7 +14,6 @@ public class AppLehen extends Application {
 
         Parse.initialize(this, "uucnk4RbZSP3o41Ntzp9Ju6o3i99ENAJqPsZYOsB", "ABhgQKHR1NNNmXqVxslL2HI1jYQNKu948MZnPapr");
         PushService.setDefaultPushCallback(this, Menua.class);
-        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 
 }
